153075423611383c201f93065798e55848aed243
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-10 14:02:42 +0900

    プリペアドステートメントを使用した記述に変更

--- process.php
+++ process.php
@@ -1,14 +1,23 @@
 <?php
 include("connect.php");
+
 if (isset($_POST["create"])) {
     $title = mysqli_real_escape_string($conn, $_POST["title"]);
     $author = mysqli_real_escape_string($conn, $_POST["author"]);
     $category = mysqli_real_escape_string($conn, $_POST["category"]);
     $description = mysqli_real_escape_string($conn, $_POST["description"]);
-    $sql = "INSERT INTO books (title, author, category, description) VALUES ('$title', '$author', '$category', '$description')";
-    if (mysqli_query($conn, $sql)) {
+
+    // プリペアドステートメントを使用
+    $stmt = $conn->prepare("INSERT INTO books (title, author, category, description) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $title, $author, $category, $description);
+
+    if ($stmt->execute()) {
         echo "データが登録されました";
     } else {
-        die("エラーが発生しました");
+        die("エラーが発生しました: " . $stmt->error);
     }
+
+    $stmt->close();
 }
+
+$conn->close();
